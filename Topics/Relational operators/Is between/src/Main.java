5a934d2e089ade52c40a9d65d6844529395a3c07
BrunaAlves <brunahalves94@gmail.com>
2023-04-07 15:42:54 +0200

    Check the sum

--- Topics/Relational operators/Is between/src/Main.java
+++ Topics/Relational operators/Is between/src/Main.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // put your code here
+        int value = scanner.nextInt();
+        int between1 = scanner.nextInt();
+        int between2 = scanner.nextInt();
+
+        if(between1>between2){
+            boolean isBetween = value <= between1 && value >= between2;
+            System.out.println(isBetween);
+        }else{
+            boolean isBetween = value <= between2 && value >= between1;
+            System.out.println(isBetween);
+            
+        }
     }
 }
