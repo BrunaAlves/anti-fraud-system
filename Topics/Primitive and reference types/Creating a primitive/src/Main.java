61a28c7bc0337f145d45664357786624221a579b
BrunaAlves <brunahalves94@gmail.com>
2023-04-14 10:45:46 +0200

    Creating a primitive

---
+++ Topics/Primitive and reference types/Creating a primitive/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int primitiveType = 100;
+        primitiveType = 1996;
+        System.out.println(primitiveType);
+    }
+}
