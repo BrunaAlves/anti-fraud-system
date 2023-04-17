4a0a2dfce4761880c1ae73efa89dd785f81aa0da
BrunaAlves <brunahalves94@gmail.com>
2023-04-17 10:47:32 +0200

    The sum of integers from a to b

--- Topics/Array/Initializing an array of longs/src/Main.java
+++ Topics/Array/Initializing an array of longs/src/Main.java
@@ -7,5 +7,9 @@ public class Main {
         long[] longNumbers = {100000000001l,100000000002l,100000000003l};
 
         System.out.println(Arrays.toString(longNumbers));
+        int q = 'q';  // 1
+        char ch2 = q; // 2
+        short s = 1;  // 3
+        char ch3 = s; // 4
     }
 }
