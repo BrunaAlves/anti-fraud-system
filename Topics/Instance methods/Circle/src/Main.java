1a811692db56497f52b4e57086983015a1391ecf
BrunaAlves <brunahalves94@gmail.com>
2023-04-25 15:22:46 +0200

    Circle

---
+++ Topics/Instance methods/Circle/src/Main.java
@@ -0,0 +1,12 @@
+
+class Circle {
+    double radius;
+
+    public double getLength() {
+        return 2 * Math.PI * radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+}
