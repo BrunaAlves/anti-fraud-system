67f4f12f56b1ca819b69faca558de459a217d80e
BrunaAlves <brunahalves94@gmail.com>
2023-04-25 11:40:38 +0200

    Complex numbers

---
+++ Topics/Constructor/Complex numbers/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+}
+
+class Complex {
+    public double real;
+    public double imaginary;
+
+    public Complex(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+}
