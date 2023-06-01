ddad43703765d90aae63d141a988fa413ed05b93
BrunaAlves <brunahalves94@gmail.com>
2023-06-01 18:23:18 +0200

    Initializing an array of characters

--- Topics/Array/Initializing an array of longs/src/Main.java
+++ Topics/Array/Initializing an array of longs/src/Main.java
@@ -7,9 +7,5 @@ public class Main {
         long[] longNumbers = {100000000001l,100000000002l,100000000003l};
 
         System.out.println(Arrays.toString(longNumbers));
-        int q = 'q';  // 1
-        char ch2 = q; // 2
-        short s = 1;  // 3
-        char ch3 = s; // 4
     }
 }
